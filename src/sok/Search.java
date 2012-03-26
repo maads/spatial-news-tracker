ee54e972506404745c7740ee00ae6f0eda9f1766
Ruben <ruben.oen@student.uib.no>
2012-03-26 17:23:36 +0200

    TEST

--- src/sok/Search.java
+++ src/sok/Search.java
@@ -15,7 +15,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
